c9029fc8943d64a63fb681ee820859f6520645ab
Shinya Kumagai <roomful.rooms@gmail.com>
2020-11-19 19:16:30 +0900

    Adjust TasksHeader

--- Sources/UI/Tasks/TasksHeader.swift
+++ Sources/UI/Tasks/TasksHeader.swift
@@ -14,24 +14,30 @@ struct TasksHeader: View {
     // TODO: review text colors.
     var body: some View {
         HStack {
-            Text(filter.label)
-                .font(.title2)
-                .fontWeight(.bold)
-                .foregroundColor(.black)
+            Button(action: {}, label: {
+                Image(systemName: "chevron.down")
+                    .renderingMode(.template)
+                    .foregroundColor(.black)
+                    .font(Font.subheadline)
+                Text(filter.label)
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+            })
             Spacer()
             Button(action: {}, label: {
                 Label(sorting.label, systemImage: "line.horizontal.3.decrease.circle.fill")
-                    .font(.title3)
+                    .font(.subheadline)
                     .foregroundColor(.black)
             })
             Button(action: {}, label: {
                 sorting.order.icon
                     .renderingMode(.template)
                     .foregroundColor(.black)
-                    .font(Font.title3.weight(.semibold))
-                    .padding(.all, 8)
+                    .font(Font.subheadline.weight(.semibold))
+                    .padding(.all, 4)
             })
-        }.padding()
+        }.padding([.vertical], 12)
     }
 }
 
@@ -44,6 +50,7 @@ struct TasksHeader_Previews: PreviewProvider {
             )
             .previewLayout(.sizeThatFits)
             .environment(\.colorScheme, .light)
+
             TasksHeader(
                 filter: .active,
                 sorting: .createdDate(order: .desc)
